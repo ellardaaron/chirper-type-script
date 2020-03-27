d8ba8ad472dee177cc621e7cae538ec0d835df38
ellardaaron <59664272+ellardaaron@users.noreply.github.com>
2020-03-27 08:57:12 -0500

    working on front end

--- src/client/views/Compose.tsx
+++ src/client/views/Compose.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
+import { useState} from 'react';
 
 const Compose: React.FC<ComposeProps> = () => {
+
+    const [username, setUsername] = useState<string>('');
+    const [message, setMessage] = useState<string>('')
+
+const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value);
+ const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(e.target.value);
+
     return (
-        <div>
-            <h1 className="text center">Compose View</h1>
-        </div>
+        <main className="container">
+            <section className="row my-2 justify-content-center">
+                <div className="col-md-8">
+                    <form action="" className="form-group p-3 shadow border rounded">
+                        <label htmlFor="username">Username</label>
+                        <input value={username} onChange={handleUsernameChange} placeholder="Username" id="username" type="text" className="form-control"/>
+                        <label htmlFor="message">Message</label>
+                        <textarea value={message} onChange={handleMessageChange} rows={8} placeholder="Chirp into the Void" className="form-control" name="message" id="message" />
+                        <button className="btn btn-outline-primary btn-block mt-3 w-75 mx-auto shadow-sm">Send Chirp Into the Void!</button>
+                    </form>
+                </div>
+            </section>
+        </main>
     );
 };
 
--- src/client/views/Home.tsx
+++ src/client/views/Home.tsx
@@ -1,12 +1,33 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
+import {} from '../utils/types';
+
+import HomeChirpCard from '../components/HomeChirpCard';
 
 const Home: React.FC<HomeProps> = () => {
+
+    const [chirps, setChirps] = useState <IChirp[]>([]);
+    
+    useEffect(() => {
+        (async () =>{
+            let res = await fetch ('/api/chirps');
+            let chirps = await res.json();
+            setChirps(chirps);
+        })();
+    }, []);
+
     return (
-        <div>
-            <h1 className="text center">Home View</h1>
-        </div>
+        <main className="container">
+            <section className="row my-2 justify-content-center">
+                {chirps.map(chirp => (
+                    <HomeChirpCard key = {`chirp-card-home-${chirp.id}`} chirp={chirp} />
+                ))}
+            </section>
+        </main>
     );
 };
+
+
 
 interface HomeProps {}
 
